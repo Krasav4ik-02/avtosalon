72d9626d95cbe061892aa230a23d5cd05f9251aa
Djalil03 <dzhalil.mukashov@gmail.com>
2023-11-04 01:34:39 +0600

    5 commit

--- home/templates/home/site/src/app/app.component.ts
+++ home/templates/home/site/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'site';
+
+
+  async sendRequest() {
+    let res = await fetch('http://localhost:8000')
+    let data = res
+    console.log(data)
+  }
+
+
 }
